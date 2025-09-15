24aeb551b198a8cbf1a0eae43b6d48b1f286421f
Šandera Filip <L20220082@prumyslovkajihlava.cz>
2025-09-15 12:52:41 +0200

    load app 01

--- FilmsList01/src/App.jsx
+++ FilmsList01/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.jsx'
+import { useEffect } from 'react';
 import './App.css'
+import { useState } from 'react';
+import {
+  Container
+} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //API url
+  const API_URL = "https://www.johanovsti.eu/RestAPI/api.php/filmy";
+  const [data, setData] = useState([]);
+
+  //Filter
+  const [name, setName] = useState("");
+  const [zanr, setZanr] = useState("");
+
+  //Array unique zanr
+  const [uzanr, setUzanr] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json)
+      .then((data) => {
+        setData(data);
+
+        //filer
+        const zanry = [... new Set(data.map((item) => item.zanr))];
+        setUzanr(zanry);
+      })
+      .catch((err) => console.log("Err: " + err));
+  }, []);
+
+  const getFilerData = () => {
+    return data.filter((item) => {
+      //Filter name
+      const containName = item.nazev.toLowerCase().includes(name.toLowerCase());
+
+      //Filter zanr
+      let pozanr = true;
+      if(zanr !== ""){
+        pozanr = (item.zanr == zanr);
+      }
+
+      return containName && pozanr;
+    })
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="FilmsList01 logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>FilmsList01</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
+      <Container>
+        <h1>React filter films</h1>
+      </Container>
     </>
   )
 }
